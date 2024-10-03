b065a8ef4fdacf826c362692db40656b329bb303
Slay James <slayjms@yahoo.com>
2024-10-03 09:38:13 -0500

    fizzy

---
+++ main.js
@@ -0,0 +1,26 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+function fizzy(x){
+  if(x / 3){
+    return "Fizz"
+  }
+}
+
+
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+
+if (typeof fizzy === "undefined") {
+  fizzy = undefined;
+}
+
+module.exports = {
+  fizzy,
+};
